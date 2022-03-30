12db3f8cc0b833750586985aebaabe27111d2bbb
Ivan Pazenko <ivpazenko@gmail.com>
2022-03-30 22:31:16 +0300

    Dynamic filters

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ function initApp() {
     document.getElementById('clear_storage').addEventListener('click', () => {
         localStorage.clear();
         loadSettings();
-        updateAppStatus();
+        updateAppStatus(true);
     })
 
     document.getElementById('invalidate_cache').addEventListener('click', () => {
         invalidateCache();
-        updateAppStatus();
+        updateAppStatus(true);
     })
 
     document.getElementById('filters_form').addEventListener('submit', (e) => {
@@ -28,41 +28,73 @@ function initApp() {
 
 
 // display data
-function updateAppStatus() {
+async function updateAppStatus(reload_data) {
     console.log('▶️ Update request')
-    listAddresses();
-    // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-}
-
-async function listAddresses() {
     let spreadsheet_id = getSetting('sheet_id');
     let range = getSetting('range');
 
-    if (spreadsheet_id && range) {
-        let values = await getData(spreadsheet_id, range)
-        if( values !== null) {
-            printData(sortValues(values));
-        } else {
-            createToast('Не вдалось відкрити таблицю', 'Можливо таблиця закрита, увійдіть в аккаут або повторіть спробу (відкрийте меню та натисніть "Оновити кеш кеш")', false)
-        }
-
-    } else {
-        createToast('Потребує уваги', 'Оновіть налаштування, щоб побачити список адрес', false)
+    if (!spreadsheet_id || !range) {
+        createToast('Потребує уваги', 'Оновіть налаштування, щоб побачити список адрес')
         document.getElementById('content').innerHTML = '';
+        return false;
+    }
+    let data = await getData(spreadsheet_id, range)
+    if (!data) {
+        console.log('🙊 No data returned from API...')
+        return false;
+    }
+
+    if (reload_data) {
+        updateDynamicSelects(data);
+    }
+
+    data = sortValues(data)
+    if (data) {
+        printData(data);
     }
 }
 
 function printData(values) {
     document.getElementById('content').innerHTML = '';
-    if (values.length > 0) {
-        values.forEach( ( row, index ) => {
-            if (typeof row[0] !== 'undefined') {
-                appendData(row, index)
-            }
-        })
-    } else {
-        createToast('Потребує уваги', 'Не знайдено данних, які відповідають критеріям у фільтрування')
+    values.forEach((row, index) => {
+        if (typeof row[0] !== 'undefined') {
+            let wrapper = document.getElementById('content');
+            let template = document.getElementById('list-group-item')
+            let element = template.content.firstElementChild.cloneNode(true);
+            element.setAttribute('data-bs-info', JSON.stringify(row));
+            element.setAttribute('data-bs-range', index);
+            element.querySelector('.message').innerText = row[0].replace(/[\n\r]/g, " ").slice(0, 30);
+            element.querySelector('.number').innerText = (typeof row[12] !== 'undefined') ? (row[12] + '/' + row[6]) : row[6];
+            element.querySelector('.number').title = (typeof row[12] !== 'undefined') ? (row[12] + ' зайнятих місць з ' + row[6]) : row[6] + ' місць вільно';
+            element.querySelector('.time').innerText = (typeof row[7] !== 'undefined') ? row[7].slice(0, 20) : '';
+            new bootstrap.Tooltip(element.querySelector('.number'))
+            wrapper.appendChild(element);
+        }
+    })
+}
+
+function updateDynamicSelects(data) {
+    let dynamic_selects = document.querySelectorAll('.fill_from_data');
+    if(dynamic_selects !== null) {
+        dynamic_selects.forEach( select => updateFilterData(data, select))
     }
+}
+
+function updateFilterData(data, select) {
+    if (select === null) {
+        return false;
+    }
+    let row_index = parseFloat(select.getAttribute('data-column-index'));
+
+    // clear options
+    select.querySelectorAll('option:not(.dont_remove)').forEach(option => option.remove())
+
+    data = data.map( row => row[row_index])
+    data = [...new Set(data)]
+    data.forEach(item => {
+        select.add(new Option(item, item));
+    })
+
 }
 
 // data storage and transformations
@@ -70,9 +102,22 @@ function sortValues(values) {
     let order = document.querySelector('[name="sort-order"]').value;
     let number = parseFloat(document.querySelector('[name="number_of_people"]').value);
     let visibility = document.querySelector('input[name="visibility_setting"]:checked').value; // all/free/busy
+    let dynamic_selects = document.querySelectorAll('.fill_from_data');
+
+    // dynamic selects
+    if(dynamic_selects !== null) {
+        dynamic_selects.forEach( select => {
+            if (select.value !== 'all') {
+                let row_index = parseFloat(select.getAttribute('data-column-index'));
+                values = values.filter(row => {
+                    return row[row_index] === select.value;
+                });
+            }
+        })
+    }
 
     // sort order
-    if (order !== 'none') {
+    if (order !== 'all') {
         values.sort((a, b) => {
             a = typeof a[6] === 'undefined' ? 0 : parseFloat(a[6]);
             b = typeof b[6] === 'undefined' ? 0 : parseFloat(b[6]);
@@ -100,26 +145,22 @@ function sortValues(values) {
                 break
         }
     }
-
-    return values;
+    if (values.length > 0) {
+        return values;
+    } else {
+        createToast('Потребує уваги', 'Не знайдено данних, які відповідають критеріям у фільтрування')
+        return false;
+    }
 }
 
 async function getData(spreadsheet_id, range) {
-    const always_update = getSetting('always_update') === 'true';
-    let data;
-
     try {
         // try to get from cache
-        data = getFromCache();
-        if (data) {
-            return data
-        } else {
-            return await getDataFromAPI(spreadsheet_id, range);
-        }
-
+        let data = getFromCache();
+        return data ? data : await getDataFromAPI(spreadsheet_id, range);
     } catch (error) {
         if (error.status === 403) {
-            createToast('Не вдалось відкрити таблицю', 'Ви намагаєтесь отримати доступ до закритого файлу. Щоб переглядати закриті файли необхідно увійти в додаток')
+            createToast('Не вдалось відкрити таблицю', 'Ви намагаєтесь отримати доступ до закритого файлу. Щоб переглядати закриті файли увійдіть в аккаут або повторіть спробу (відкрийте меню та натисніть "Оновити кеш кеш")', false)
         } else {
             createToast('🛑Сталась помилка', 'Якщо проблема не вирішилась сама-собою та перешкоджає роботі напишіть розробнику за яких обставин вона виникає', false)
             console.log(error)
@@ -129,15 +170,15 @@ async function getData(spreadsheet_id, range) {
 }
 
 function getFromCache() {
-    const MILLISECONDS_TO_MINUTES = 1000*60;
+    const MILLISECONDS_TO_MINUTES = 1000 * 60;
     let savedTime = parseFloat(getSetting('cached_time'));
     let now = Date.now();
     let invalidateTime = (getSetting('short_life_cache') === 'true') ? 2 : 10; // minutes
 
-    let isOutdated = isNaN(savedTime) ? true : Math.floor((now - savedTime)/MILLISECONDS_TO_MINUTES ) > invalidateTime;
+    let isOutdated = isNaN(savedTime) ? true : Math.floor((now - savedTime) / MILLISECONDS_TO_MINUTES) > invalidateTime;
     let raw_data = getSetting('api_cache');
 
-    if ( !isOutdated ) {
+    if (!isOutdated) {
         if (raw_data !== null) {
             let data = JSON.parse(raw_data);
             if (data.length > 0) {
@@ -180,18 +221,23 @@ async function getDataFromAPI(spreadsheet_id, range) {
     })
     data = data.result.values
     if (data.length === 0) {
-        createToast('Потребує уваги', 'У вказаній таблиці не знайдено данні. Оберінь іншу таблицю або змініть межі таблиці')
+        createToast('Потребує уваги', 'У вказаній таблиці не знайдено данні. Оберінь іншу таблицю або змініть межі таблиці');
+        return false;
+    } else {
+        saveToCache(data)
+        data = data.filter(row => row !== null)
+        return data;
     }
-    saveToCache(data)
-    return data;
 }
 
-function getSheetLink(index= 0) {
+function getSheetLink(index = 0) {
     let range;
+    let current_sheet_id = 0;
     let start = getSetting('range').split(':')[0];
-    index = ( ( getSetting('first_row_names') === 'true' ) ? 1 : 0 ) + parseFloat(index) + parseFloat(start.match(/\d+/)[0]);
+    index = ((getSetting('first_row_names') === 'true') ? 1 : 0) + parseFloat(index) + parseFloat(start.match(/\d+/)[0]);
     range = start[0] + index;
-    return 'https://docs.google.com/spreadsheets/d/'+getSetting('sheet_id')+'/edit#gid=0&range=' + range;
+
+    return 'https://docs.google.com/spreadsheets/d/' + getSetting('sheet_id') + '/edit#gid=' + current_sheet_id + '&range=' + range;
 }
 
 // settings
@@ -234,20 +280,6 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
-function appendData(row, range = '') {
-    let wrapper = document.getElementById('content');
-    let template = document.getElementById('list-group-item')
-    let element = template.content.firstElementChild.cloneNode(true);
-    element.setAttribute('data-bs-info', JSON.stringify(row));
-    element.setAttribute('data-bs-range', range);
-    element.querySelector('.message').innerText = row[0].replace(/[\n\r]/g, " ").slice(0,30);
-    element.querySelector('.number').innerText = (typeof row[12] !== 'undefined') ? (row[12] + '/' + row[6]) : row[6];
-    element.querySelector('.number').title = (typeof row[12] !== 'undefined') ? (row[12] + ' зайнятих місць з ' + row[6]) : row[6] + ' місць вільно';
-    element.querySelector('.time').innerText = (typeof row[7] !== 'undefined') ? row[7].slice(0,20) : '';
-    new bootstrap.Tooltip(element.querySelector('.number'))
-    wrapper.appendChild(element);
-}
 
 function createToast(header = '', message = '', autoDismiss = true) {
     let toastContainer = document.querySelector('.toast-container');
@@ -320,6 +352,7 @@ function updateInfoModal() {
             discoveryDocs: DISCOVERY_DOCS,
             scope: SCOPES
         }).then(function () {
+            updateAppStatus(true);
             // Listen for sign-in state changes.
             gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
@@ -340,7 +373,6 @@ function updateInfoModal() {
     function updateSigninStatus(isSignedIn) {
         authorizeButton.classList.toggle('d-none', isSignedIn)
         signoutButton.classList.toggle('d-none', !isSignedIn)
-        listAddresses();
     }
 
     /**
@@ -374,5 +406,3 @@ function updateInfoModal() {
         // });
     }
 }
-
-
